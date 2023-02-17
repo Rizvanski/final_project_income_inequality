626d1a79b03b158878fbe95c66e5abb0c376a2c5
Rizvanski <haris.rizvanski@live.com>
2023-02-17 18:29:31 +0100

    'Initial commit.'

---
+++ src/financial_development_and_income_inequality/__init__.py
@@ -0,0 +1,1 @@
+"""Code for producing the results of the Financial Development and Income Inequality project."""
---
+++ src/financial_development_and_income_inequality/data_management/__init__.py
@@ -0,0 +1,5 @@
+"""Functions for managing data."""
+
+from financial_development_and_income_inequality.data_management.clean_data import clean_data
+
+__all__ = [clean_data]
